5dfb3825b1e785de70ce87b4fadd701f2dad27f8
dlgs5100 <dlgs5100@gmail.com>
2017-08-28 11:13:23 +0800

    First modify

---
+++ Does my number look big in this/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Does my number look big in this")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Does my number look big in this")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c953af5-25ec-4dba-b982-b008bf7016db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Does my number look big in this/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+
+namespace Does_my_number_look_big_in_this
+{
+    [TestFixture]
+    public class Sample_Test
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(1)
+                                .Returns(true)
+                                .SetDescription("1 is narcissitic");
+                yield return new TestCaseData(371)
+                                .Returns(true)
+                                .SetDescription("371 is narcissitic");
+
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(int n) => Kata.Narcissistic(n);
+    }
+    public class Kata
+    {
+        public static bool Narcissistic(int value)
+        {
+            var result = value.ToString().Select(i => Math.Pow((int)Char.GetNumericValue(i), value.ToString().Length)).Sum();
+
+            return value == result ? true : false;
+        }
+    }
+}
